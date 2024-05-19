eaa6f2f5f7e0708cf2160a6e01aacc4a617144a1
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2024-05-18 21:26:49 -0300

    Add files via upload

---
+++ javascript/pratica-js/exercicios_gerais/quociente-resto/js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let primeiroValor,
+    segundoValor
+
+primeiroValor = Number(prompt("Digite o 1º valor: "))
+segundoValor = Number(prompt("Digite o 2º valor: "))
+
+console.log (`O quociente entre ${primeiroValor} e ${segundoValor} é ${primeiroValor / segundoValor}`)
+console.log (`O resto entre ${primeiroValor} e ${segundoValor} é ${primeiroValor % segundoValor}`)
