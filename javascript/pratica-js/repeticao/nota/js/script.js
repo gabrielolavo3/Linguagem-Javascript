eb020000df95945c9feb30c01c882155fc0223e9
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2024-05-21 21:53:28 -0300

    Update script.js

--- javascript/pratica-js/repeticao/nota/js/script.js
+++ javascript/pratica-js/repeticao/nota/js/script.js
@@ -1,3 +1,7 @@
+/* 
+Criar um programa que peça uma nota, entre zero e dez. 
+Exibir uma mensagem caso o valor seja inválido e continuar pedindo até que o usuário informe um valor válido. */
+
 nota = Number(prompt("Digite a nota: "))
 
 while (nota < 0 || nota > 10) {
